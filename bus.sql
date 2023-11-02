0f527ac4ba2589678a8fc36fa698e6dd05516987
Suresh S Vishwakarma <148203314+vssuresha@users.noreply.github.com>
2023-11-02 13:04:35 +0530

    Add files via upload

---
+++ bus.sql
@@ -0,0 +1,1 @@
+Create database busresv;use busresv;create table bus( id int primary key,ac boolean, capacity int); insert into bus values (1,1,2);insert into bus values (2,1,48);insert into bus values(3,0,52); select * from bus;create table booking(  passenger name varchar(50), bus_no int,  travel_date date);   select * from booking;
